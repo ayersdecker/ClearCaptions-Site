ca078365b7cbdd81cd7e916d1141cd06b0eb64bf
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-08 19:53:18 +0000

    New JS

--- docs/assets/script/caption.js
+++ docs/assets/script/caption.js
@@ -7,18 +7,15 @@ for (let i = 0; i < maxLines; i++) {
   captionLines.push('');
 }
 
-// Set up Web Audio API context
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const analyser = audioContext.createAnalyser();
 
-// Set up WebRTC API to capture audio
 navigator.mediaDevices
   .getUserMedia({ audio: true })
   .then(stream => {
     const audioSource = audioContext.createMediaStreamSource(stream);
     audioSource.connect(analyser);
 
-    // Analyze audio data and update captions
     analyser.fftSize = 256;
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
@@ -26,12 +23,7 @@ navigator.mediaDevices
     const updateAudioCaptions = () => {
       analyser.getByteFrequencyData(dataArray);
 
-
-      // Update the audio caption element with the visualization
-      audioCaptionElement.innerHTML = audioVisualization;
-      requestAnimationFrame(updateAudioCaptions);
     };
-
     updateAudioCaptions();
   })
   .catch(error => {
