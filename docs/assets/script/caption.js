db707ac5205cfd47d1ba032e1f0c4a8367349570
Decker Ayers <69859630+ayersdecker@users.noreply.github.com>
2023-08-08 20:00:19 +0000

    js

--- docs/assets/script/caption.js
+++ docs/assets/script/caption.js
@@ -1,31 +1,28 @@
-const micCaptionElement = document.getElementById('mic-caption');
-const audioCaptionElement = document.getElementById('audio-caption');
-const maxLines = 3;
-const captionLines = [];
+const captionElement = document.getElementById('caption');
 
-for (let i = 0; i < maxLines; i++) {
-  captionLines.push('');
+// Check if the browser supports the Web Speech API
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+  // Set continuous to true for live captions
+  recognition.continuous = true;
+
+  // When speech recognition returns a result
+  recognition.onresult = (event) => {
+    const lastResultIndex = event.results.length - 1;
+    const lastResult = event.results[lastResultIndex][0].transcript;
+
+    // Update the caption text with the latest recognized speech
+    captionElement.textContent = lastResult;
+  };
+
+  // When an error occurs
+  recognition.onerror = (event) => {
+    console.error('Speech recognition error:', event.error);
+  };
+
+  // Start speech recognition
+  recognition.start();
+} else {
+  captionElement.textContent = 'Speech recognition is not supported in this browser.';
 }
-
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const analyser = audioContext.createAnalyser();
-
-navigator.mediaDevices
-  .getUserMedia({ audio: true })
-  .then(stream => {
-    const audioSource = audioContext.createMediaStreamSource(stream);
-    audioSource.connect(analyser);
-
-    analyser.fftSize = 256;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    const updateAudioCaptions = () => {
-      analyser.getByteFrequencyData(dataArray);
-
-    };
-    updateAudioCaptions();
-  })
-  .catch(error => {
-    console.error('Error capturing audio:', error);
-  });
